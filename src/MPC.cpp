9fc188289ca6d011211acd13aadd37584823c288
Varun Rao <vrao_87@Varuns-MacBook-Air.local>
2018-01-21 00:37:41 +0530

    Improvements

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -54,9 +54,9 @@ class FG_eval {
 
     // The part of the cost based on the reference state.
     for (int t = 0; t < N; t++) {
-      fg[0] += 10 * CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 2 * CppAD::pow(vars[epsi_start + t], 2);
-      fg[0] += 2 * CppAD::pow(vars[v_start + t] - ref_v, 2);
+      fg[0] += 50 * CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 10 * CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 1 * CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
@@ -67,7 +67,7 @@ class FG_eval {
 
     // Minimize the value gap between sequential actuations.
     for (int t = 0; t < N - 2; t++) {
-      fg[0] += 0.1 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 100 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += 0.001 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
