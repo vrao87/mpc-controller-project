1aa56be7958bcabded3aaf1af1a947b454d5dac3
Varun Rao <vrao_87@Varuns-MacBook-Air.local>
2018-01-21 12:46:09 +0530

    fine tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,7 +6,7 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 10;
+size_t N = 12;
 double dt = 0.1;
 
 // Both the reference cross track and orientation errors are 0.
